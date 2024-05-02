857f6385e20b022fa33139f27ab10e2cf6de883e
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-05-02 16:14:31 -0300

    finalizando pagina home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,24 @@
-import { Container } from "./style";
-import bgImg from '../../assets/home/background-home-mobile.jpg';
+import { Container, Main } from "./style";
+import bgImg from "../../assets/home/background-home-mobile.jpg";
 
 export const Home = () => {
   return (
     <Container>
-     <img className="imgFundo" src={bgImg} alt="Imagem de fundo" />
+      <img className="imgFundo" src={bgImg} alt="Imagem de fundo" />
+      <Main>
+        <p className="title">SO, YOU WANT TO TRAVEL TO</p>
+        <h1>SPACE</h1>
+        <p className="text">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
 
+        <div className="circulo">
+          <p>EXPLORE</p>
+        </div>
+      </Main>
     </Container>
-  )
-}
+  );
+};
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -1,10 +1,46 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
+    .imgFundo{
+        position: absolute;
+        z-index: -1;
+        left: 0;
+        right: 0;
+        height: 100vh;
+    }
+`;
 
- 
-    
 
-    
+export const Main = styled.div`
+    text-align: center;
+    padding: 11.2rem 2.4rem 0;
+
+    .title{
+        color: var(--azul-pastel);
+    }
+
+    h1{
+        color: var(--branco);
+        margin: 1.6rem auto;
+    }
+
+    .text{
+        font-size: 1.5rem;
+        color: var(--azul-pastel);
+        margin-bottom: 9.4rem;
+    }
+
+    .circulo{
+        background: var(--branco);
+        border-radius: 50rem;
+        height: 15rem;
+        width: 15rem;
+        margin: 0 auto;
+
+        padding: 6.5rem 3.4rem;
+
+        p{
+            font-size: 2rem;
+        }
+    }
 `;
