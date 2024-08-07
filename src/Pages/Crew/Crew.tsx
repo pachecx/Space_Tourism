4a3a88eb8d40fbdb1c02c5422dd088b15311313c
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-08-07 17:07:21 -0300

    page Crew

--- src/Pages/Crew/Crew.tsx
+++ src/Pages/Crew/Crew.tsx
@@ -1,11 +1,81 @@
-//import { Container } from "./style";
+import astro from "../../assets/crew/image-douglas-hurley.png";
+import specialist from "../../assets/crew/image-mark-shuttleworth.png";
+
+import imgM from "../../assets/crew/background-crew-mobile.jpg";
+import imgTa from "../../assets/crew/background-crew-tablet.jpg";
+import imgDesk from "../../assets/crew/background-crew-desktop.jpg";
+
+import { Container } from "./style";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+import { Responsividade } from "../../components/Responsividade";
 
 export const Crew = () => {
   return (
-    <div>
-      <main>
-        
-      </main>
-    </div>
-  )
-}
+    <Container>
+      <Responsividade bgImgM={imgM} bgImgDesk={imgDesk} bgImgTablet={imgTa} />
+      {/* <img className="bgIMg" src={imgM} alt="" /> */}
+
+      <p className="textTop">
+        <span className="numberTop">02</span>
+        Meet your crew
+      </p>
+
+      <div className="">
+        <main>
+          <Swiper
+            pagination={{
+              el: ".swiper-pagination",
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide className="swipeContent">
+              <img className="imgSwiper" src={astro} alt="" />
+              <div className="line" />
+              <div className="text">
+                <p className="textPosition">commander</p>
+
+                <h2 className="textName">Douglas Hurley</h2>
+                <p className="textAbout">
+                  Douglas Gerald Hurley is an American engineer, former Marine
+                  Corps pilot and former NASA astronaut. He launched into space
+                  for the third time as commander of Crew Dragon Demo-2.
+                </p>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className="swipeContent">
+              <img className="imgSwiper" src={specialist} alt="" />
+              <div className="line" />
+              <div className="text">
+                <p className="textPosition">Mission Specialist </p>
+
+                <h2 className="textName">MARK SHUTTLEWORTH</h2>
+                <p className="textAbout">
+                  Mark Richard Shuttleworth is the founder and CEO of Canonical,
+                  the company behind the Linux-based Ubuntu operating system.
+                  Shuttleworth became the first South African to travel to space
+                  as a space tourist.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+
+            <div className="swiper-pagination"></div>
+          </Swiper>
+        </main>
+      </div>
+    </Container>
+  );
+};
--- src/Pages/Crew/style.ts
+++ src/Pages/Crew/style.ts
@@ -1,5 +1,83 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  border: 1px solid #000;
+  height: 100vh;
 
+  .bgIMg {
+    position: absolute;
+    z-index: -1;
+    left: 0;
+  }
+
+  .swiper-pagination {
+    bottom: 190px; /* Ajuste conforme necessário */
+    left: 50%;
+    transform: translateX(-50%);
+    position: absolute;
+  }
+  
+  .swiper-pagination-bullet {
+  width: 10px;
+  height: 10px;
+  text-align: center;
+  line-height: 20px;
+  font-size: 12px;
+  color: #000;
+  opacity: 1;
+  background: #56585e;
+}
+
+  .swiper-pagination-bullet-active {
+  color: #fff;
+  background: #ffff;
+}
+
+  .textTop {
+    font-size: 1.6rem;
+    color: #56585e;
+    font-weight: bold;
+    text-align: center;
+    margin: 8.8rem auto 1.6rem;
+    font-family: 'Barlow Condensed', sans-serif;
+  }
+
+  .numberTop {
+    font-size: 1.6rem;
+    color: #56585e;
+    margin-right: 1.6rem;
+  }
+
+  main {
+    .swipeContent {
+      //height: 100vh;
+
+      text-align: center;
+    }
+
+    img {
+      height: 22.2rem;
+      width: 17.7rem;
+      margin: 0 auto;
+    }
+
+    .text {
+      margin-top: 3.2rem;
+      padding: 0 2.4rem;
+
+      .textPosition {
+        color: #56585e;
+        font-size: 2.6rem;
+      }
+
+      .textName {
+        color: #ffff;
+        font-size: 2.4rem;
+      }
+
+      .textAbout {
+        color: #d0d6f9;
+      }
+    }
+  }
 `;
